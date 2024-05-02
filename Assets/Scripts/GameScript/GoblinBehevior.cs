5f62f9a48acdf1eb00822b85eab16c838ecf88f5
Yann Fournier <y.fourni@icloud.com>
2024-05-02 17:38:35 +0200

    ajout du nav mesh pour les IA des monstres

--- Assets/Scripts/GameScript/GoblinBehevior.cs
+++ Assets/Scripts/GameScript/GoblinBehevior.cs
@@ -2,45 +2,39 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GoblinBehevior : MonoBehaviour
 {
-    [SerializeField] float Speed;
     
     [SerializeField] Animator GoblinAnimator;
 
     private Rigidbody _rb;
     private Transform _t;
     private CapsuleCollider _c;
-    private UnityEngine.AI.NavMeshAgent _n;
+    private NavMeshAgent _n;
     private Vector3 _startPosition;
-    // Start is called before the first frame update
+    
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _t = GetComponent<Transform>();
         _c = GetComponent<CapsuleCollider>();
-        _n = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        _startPosition = _t.GetChild(0).transform.position;
+        _n = GetComponent<NavMeshAgent>();
+        _startPosition = _t.GetComponentInParent<Transform>().position;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void OnTriggerStay(Collider other)
     {
-        if (other.transform.tag == "Player")
+        if (other.tag == "Player")
         {
-            _n.SetDestination(other.transform.position);
+            _n.SetDestination(GameManager.Instance.Player.GetComponent<Transform>().position);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.transform.tag == "Player")
+        if (other.tag == "Player")
         {
             _n.SetDestination(_startPosition);
         }
